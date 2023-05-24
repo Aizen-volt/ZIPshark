6d3ebee939924cecf421067db28bae5dc1339427
Mateusz Kaszubowski <119121964+Aizen-volt@users.noreply.github.com>
2023-05-24 20:50:37 +0200

    added dictionary mode

--- zipshark.sh
+++ zipshark.sh
@@ -2,7 +2,7 @@
 # Created On       : 2023-04-25
 # Last Modified By : Mateusz Kaszubowski ( 193050 )
 # Last Modified On : 2023-05-23
-# Version          : prealpha 0.0.4
+# Version          : prealpha 0.0.6
 #
 # Description      :
 #
@@ -14,7 +14,7 @@
 
 
 # META
-VERSION="prealpha 0.0.4"
+VERSION="prealpha 0.0.6"
 
 
 #EXECUTION MANAGEMENT
@@ -27,8 +27,8 @@ CAPITAL="true"
 DIGITS="true"
 SPECIAL="true"
 VERBAL="false"
-
-
+DICTIONARY=""
+DEFAULT_DICTIONARY="realhuman_phill.txt"
 
 
 function checkDependencies {
@@ -46,6 +46,63 @@ function checkDependencies {
 	fi
 }
 
+
+function fetchDictionary {
+	DICTIONARY_EXISTS="true"
+
+	if [ ! -d "/var/lib/zipshark" ]; then
+		echo "Dictionary directory not found!"
+		sudo mkdir /var/lib/zipshark
+		DICTIONARY_EXISTS="false"
+	fi
+	TEMP=$(find /var/lib/zipshark -name $DEFAULT_DICTIONARY -type f)
+	if [[ "$TEMP" == "" ]]; then
+		DICTIONARY_EXISTS="false"
+	fi
+
+	if [[ "$DICTIONARY_EXISTS" == "false" ]]; then
+		echo "Default dictionary not found!"
+		echo "Do you want to download it? (y/n)"
+		read -n 1 -s -r INPUT
+		if [[ "$INPUT" == "y" ]]; then
+			printTitle
+			sudo wget https://crackstation.net/files/crackstation-human-only.txt.gz
+			if [[ "$?" != "0" ]]; then
+				clear
+				echo "An error occured while downloading dictionary!"
+				exit 3
+			fi
+			sudo mv crackstation-human-only.txt.gz /var/lib/zipshark/$DEFAULT_DICTIONARY.gz
+			sudo gunzip /var/lib/zipshark/$DEFAULT_DICTIONARY.gz
+			DICTIONARY=/var/lib/zipshark/$DEFAULT_DICTIONARY
+		else
+			echo "No dictionary selected!"
+			sleep 2
+			return
+		fi
+	else
+		DICTIONARY=/var/lib/zipshark/$DEFAULT_DICTIONARY
+	fi
+}
+
+
+function printPasswordFound {
+	printTitle
+	echo "Password found!"
+	echo "Password: $1"
+	echo "Time elapsed: $2 seconds"
+	echo ""
+	exit 0
+}
+
+
+function printPasswordNotFound {
+	printTitle
+	echo "Password could not be found! :(("
+	echo "Try adjusting search parameters"
+	echo ""
+	exit 4
+}
 
 
 function print_version {
@@ -80,14 +137,103 @@ function print_help {
 }
 
 
-function is_positive_number {
-	if [[ $1 =~ ^[0-9]+$ ]]
-	then
-		echo "$1"
-	else
-		echo "INVALID PARAMETER $1. INTEGER VALUE WAS EXPECTED"
-		exit 1
-	fi
+function dictionaryMenu {
+	INPUT=""
+	while [[ "$INPUT" != "3" ]]; do
+		printTitle
+		echo "Dictionary mode"
+		echo "If there is no dictionary specified, built-in one will be used"
+		echo "Dictionary file should contain one word per line - otherwise recovery process may not work as intended"
+		echo "Beware that turning on verbal mode significantly slows down the process"
+		echo ""
+		echo "1. Dictionary file: $DICTIONARY"
+		echo "2. Verbal mode: $VERBAL"
+		echo "3. Run"
+		echo ""
+		echo "4. Return to main menu"
+		echo ""
+		read -p "Choose option: " INPUT
+		case $INPUT in
+			1)
+				printTitle
+				read -p "Enter dictionary file directory: " INPUT
+				TEMP=$(find -name $INPUT -type f)
+				if [[ "$TEMP" == "" ]]; then
+					echo "File not found!"
+					sleep 2
+				else
+					DICTIONARY=$TEMP
+				fi
+				;;
+			2)
+				if [[ "$VERBAL" == "true" ]]; then
+					VERBAL="false"
+				else
+					VERBAL="true"
+				fi
+				;;
+			3)	
+				if [[ "$DICTIONARY" == "" ]]; then
+					fetchDictionary
+				fi
+				if [[ "$DICTIONARY" == "" ]]; then
+					continue
+				fi
+				if [[ "$VERBAL" == "true" ]]; then
+					dictionaryVerbal
+				else
+					dictionary
+				fi
+				;;
+			4)
+				return
+				;;
+			*)
+				echo "Invalid option!"
+				sleep 2
+				;;
+		esac
+	done
+}
+
+
+function dictionary {
+	clear
+	start=`date +%s`
+	while read -r password; do
+		unzip -qq -o -P $password $FILE 2>/dev/null
+		exitcode=$? >> 8
+		if [[ "$exitcode" == "0" ]]; then
+			end=`date +%s`
+			runtime=$((end-start))
+			printPasswordFound $password $runtime
+		fi
+	done < $DICTIONARY
+	printPasswordNotFound
+	exit 2
+}
+
+
+function dictionaryVerbal {
+	LINES_COUNT=$(wc -l < $DICTIONARY)
+	COUNTER=0
+	clear
+	start=`date +%s`
+	while read -r password; do
+		clear
+		COUNTER=$((COUNTER+1))
+		PERCENTAGE=$((COUNTER*100/LINES_COUNT))
+		echo "Checked $COUNTER/$LINES_COUNT ($PERCENTAGE%)"
+		echo "Checking: $password"
+		unzip -qq -o -P $password $FILE 2>/dev/null
+		exitcode=$? >> 8
+		if [[ "$exitcode" == "0" ]]; then
+			end=`date +%s`
+			runtime=$((end-start))
+			printPasswordFound $password $runtime
+		fi
+	done < $DICTIONARY
+	printPasswordNotFound
 }
 
 
@@ -144,6 +290,10 @@ function bruteforceMenu {
 					fi
 				else
 					echo "Invalid parameter! Non-negative integer value expected"
+					sleep 2
+				fi
+				if [[ $MAX_LENGTH -gt 128 ]]; then
+					echo "Max length cannot be greater than 128"
 					sleep 2
 				fi
 				;;
@@ -205,7 +355,18 @@ function bruteforceMenu {
 					VERBAL="true"
 				fi
 				;;
-			5)
+			5)	
+				if [[ "$SMALL" == "false" && "$CAPITAL" == "false" && "$DIGITS" == "false" && "$SPECIAL" == "false" ]]; then
+					echo "Setting each charset range to false is not allowed!"
+					sleep 2
+					continue
+				fi
+				if [[ "$MIN_LENGTH" == "" ]]; then
+					MIN_LENGTH="1"
+				fi
+				if [[ "$MAX_LENGTH" == "" ]]; then
+					MAX_LENGTH="128"
+				fi
 				if [[ "$VERBAL" == "true" ]]; then
 					bruteforceVerbal
 				else
@@ -240,18 +401,15 @@ function bruteforce {
 	clear
 	start=`date +%s`
 	while read -r password; do
-		unzip -qq -o -P $password test.zip 2>/dev/null
+		unzip -qq -o -P $password $FILE 2>/dev/null
 		exitcode=$? >> 8
 		if [[ "$exitcode" == "0" ]]; then
 			end=`date +%s`
 			runtime=$((end-start))
-			echo "Password found: $password"
-			echo "Time elapsed: $runtime seconds"
-			exit 0
+			printPasswordFound $password $runtime
 		fi
 	done < <(crunch $MIN_LENGTH $MAX_LENGTH $CHARSET)
-	echo "Password not found"
-	exit 0
+	printPasswordNotFound
 }
 
 
@@ -270,48 +428,42 @@ function bruteforceVerbal {
 		CHARSET=$CHARSET"!\"#$%&'()*+,-./:;<=>?@[\]^_\`{|}~"
 	fi
 
-	CHARSET_LENGTH=${#CHARSET}
-	CHARSET_LENGTH=$((CHARSET_LENGTH-2))
-	COMBINATIONS=0
-	for (( i=$MIN_LENGTH - 1; i<$MAX_LENGTH; i++ )); do
-		TEMP=1
-		for (( j=0; j<=$i; j++ )); do
-			TEMP=$((TEMP*CHARSET_LENGTH))
+	if [[  "$MAX_LENGTH" != "128" ]]; then
+		CHARSET_LENGTH=${#CHARSET}
+		CHARSET_LENGTH=$((CHARSET_LENGTH-2))
+		COMBINATIONS=0
+		for (( i=$MIN_LENGTH - 1; i<$MAX_LENGTH; i++ )); do
+			TEMP=1
+			for (( j=0; j<=$i; j++ )); do
+				TEMP=$((TEMP*CHARSET_LENGTH))
+			done
+			COMBINATIONS=$((COMBINATIONS+TEMP))
 		done
-		COMBINATIONS=$((COMBINATIONS+TEMP))
-	done
+	fi
+	
 	COUNTER=0
 	clear
 	start=`date +%s`
 	while read -r password; do
 		clear
 		COUNTER=$((COUNTER+1))
-		PERCENTAGE=$((COUNTER*100/COMBINATIONS))
-		echo "Checked $COUNTER/$COMBINATIONS ($PERCENTAGE%)"
+		if [[  "$MAX_LENGTH" != "128" ]]; then
+			PERCENTAGE=$((COUNTER*100/COMBINATIONS))
+			echo "Checked $COUNTER/$COMBINATIONS ($PERCENTAGE%)"
+		else
+			echo "Checked $COUNTER"
+		fi
 		echo "Checking: $password"
-		unzip -qq -o -P $password test.zip 2>/dev/null
+		unzip -qq -o -P $password $FILE 2>/dev/null
 		exitcode=$? >> 8
 		if [[ "$exitcode" == "0" ]]; then
 			end=`date +%s`
 			runtime=$((end-start))
-			echo "Password found: $password"
-			echo "Time elapsed: $runtime seconds"
-			exit 0
+			printPasswordFound $password $runtime
 		fi
 	done < <(crunch $MIN_LENGTH $MAX_LENGTH $CHARSET)
-	echo "Password not found"
-	exit 0
+	printPasswordNotFound
 }
-
-
-#function dictionary {
-
-#}
-
-
-#function dictionaryMenu {
-
-#}
 
 
 function printTitle {
@@ -399,7 +551,12 @@ function toggleMainMenu {
 					fi
 				fi
 				;;
-			3)
+			3)	
+				if [[ "$FILE" == "not selected" ]]; then
+					echo "No file selected!"
+					sleep 2
+					continue
+				fi
 				if [[ "$EXECUTION_MODE" == "bruteforce" ]]; then
 					bruteforceMenu
 				elif [[ "$EXECUTION_MODE" == "dictionary" ]]; then
